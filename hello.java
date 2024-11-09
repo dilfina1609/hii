fb8757c17c6aa682ba9c1c590b2c001b7e75c2fa
dilfina1609 <124345099+dilfina1609@users.noreply.github.com>
2024-11-09 15:03:32 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@
 public class hello{
     public static void main(String args[])
     {
-        System.out.println("Hello, Welcome to Jenkins");
+        System.out.println("Hello, Welcome to Full Stack");
     }
 }
 
